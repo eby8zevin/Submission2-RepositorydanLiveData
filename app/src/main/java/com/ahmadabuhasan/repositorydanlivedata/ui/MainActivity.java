2a84f72409805c9e02d977cdee47b46ccbfc7dcc
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-01-18 21:53:48 +0700

    TAB + backPressed + Toast

--- app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/ui/MainActivity.java
+++ app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/ui/MainActivity.java
@@ -1,16 +1,55 @@
 package com.ahmadabuhasan.repositorydanlivedata.ui;
 
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.ahmadabuhasan.repositorydanlivedata.R;
+import com.ahmadabuhasan.repositorydanlivedata.databinding.ActivityMainBinding;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
+
+    @StringRes
+    private final int[] TAB_TITLES = new int[]{
+            R.string.movies,
+            R.string.tv_show
+    };
+
+    private long backPressedTime;
+    private Toast backToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        MainPagerAdapter mainPagerAdapter = new MainPagerAdapter(this);
+        binding.viewPager2.setAdapter(mainPagerAdapter);
+        new TabLayoutMediator(binding.tabLayout, binding.viewPager2,
+                (tab, position) -> tab.setText(getResources().getString(TAB_TITLES[position]))
+        ).attach();
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setElevation(0);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (backPressedTime + 3000 > System.currentTimeMillis()) {
+            backToast.cancel();
+            super.onBackPressed();
+            return;
+        } else {
+            backToast = Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT);
+            backToast.show();
+        }
+        backPressedTime = System.currentTimeMillis();
     }
 }
