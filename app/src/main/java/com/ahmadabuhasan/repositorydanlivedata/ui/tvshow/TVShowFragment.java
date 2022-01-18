ae7717c4a91a345b06c951e9af598044b09b34bd
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-01-18 21:59:05 +0700

    TVShow Fragment

--- app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/ui/tvshow/TVShowFragment.java
+++ app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/ui/tvshow/TVShowFragment.java
@@ -2,5 +2,5 @@ package com.ahmadabuhasan.repositorydanlivedata.ui.tvshow;
 
 import androidx.fragment.app.Fragment;
 
-public class TVShowFragment extends Fragment {
+public class TVShowFragment extends Fragment                                                    {
 }
