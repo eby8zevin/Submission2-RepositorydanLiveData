7c6c1ad9033c28118b68acfee9b0b3040028bb03
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-01-19 21:44:02 +0700

    Adapter Movie

---
+++ app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/ui/movie/MovieAdapter.java
@@ -0,0 +1,31 @@
+package com.ahmadabuhasan.repositorydanlivedata.ui.movie;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+    @NonNull
+    @Override
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MovieViewHolder extends RecyclerView.ViewHolder {
+        public MovieViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
