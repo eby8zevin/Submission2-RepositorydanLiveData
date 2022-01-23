c4c0f5fc6e9a42e14c8b79a8ac001a59e79ef9c0
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-01-23 11:17:52 +0700

    Adapter Movie

--- app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/ui/movie/MovieAdapter.java
+++ app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/ui/movie/MovieAdapter.java
@@ -6,9 +6,25 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.ahmadabuhasan.repositorydanlivedata.R;
+import com.ahmadabuhasan.repositorydanlivedata.data.source.local.entity.MovieEntity;
 import com.ahmadabuhasan.repositorydanlivedata.databinding.ItemListBinding;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+
+    private final List<MovieEntity> listMovie = new ArrayList<>();
+
+    void setMovie(List<MovieEntity> listMovie) {
+        if (listMovie == null) return;
+        this.listMovie.clear();
+        this.listMovie.addAll(listMovie);
+    }
+
     @NonNull
     @Override
     public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -18,16 +34,21 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
 
     @Override
     public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
-
+        MovieEntity movieEntity = listMovie.get(position);
+        Glide.with(holder.itemView.getContext())
+                .load(movieEntity.getMoviePoster())
+                .apply(RequestOptions.placeholderOf(R.drawable.ic_loading).error(R.drawable.ic_error))
+                .into(holder.binding.ivPoster);
+        holder.binding.tvTitle.setText(movieEntity.getMovieTitle());
+        holder.binding.tvRelease.setText(movieEntity.getMovieReleaseDate());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listMovie.size();
     }
 
-    public class MovieViewHolder extends RecyclerView.ViewHolder {
-
+    public static class MovieViewHolder extends RecyclerView.ViewHolder {
         private final ItemListBinding binding;
 
         public MovieViewHolder(@NonNull ItemListBinding binding) {
