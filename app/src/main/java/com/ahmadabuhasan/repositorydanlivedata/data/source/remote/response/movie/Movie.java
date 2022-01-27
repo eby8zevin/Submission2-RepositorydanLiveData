455a2b663af65ff114c1494ea6b37f48d40a3909
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-01-28 05:54:04 +0700

    Response Movie

--- app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/data/source/remote/response/movie/Movie.java
+++ app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/data/source/remote/response/movie/Movie.java
@@ -8,25 +8,43 @@ public class Movie {
 
     @SerializedName("adult")
     private Boolean adult;
+
     @SerializedName("backdrop_path")
     private String backdropPath;
+
     @SerializedName("genre_ids")
-    private List<int> genreIds;
+    private List<Integer> genreIds;
+
     @SerializedName("id")
     private int id;
+
     @SerializedName("original_language")
     private String originalLanguage;
+
     @SerializedName("original_title")
     private String original_time;
+
     @SerializedName("overview")
-    private String ("overview");
+    private String overview;
+
     @SerializedName("popularity")
     private Double popularity;
-    @SerializedName("poster_path")
-    @SerializedName("release_date")
-    @SerializedName("title")
-    @SerializedName("video")
-    @SerializedName("vote_average")
-    @SerializedName("vote_acount")
 
+    @SerializedName("poster_path")
+    private String posterPath;
+
+    @SerializedName("release_date")
+    private String releaseDate;
+
+    @SerializedName("title")
+    private String title;
+
+    @SerializedName("video")
+    private Boolean video;
+
+    @SerializedName("vote_average")
+    private Double voteAverage;
+
+    @SerializedName("vote_count")
+    private int voteCount;
 }
