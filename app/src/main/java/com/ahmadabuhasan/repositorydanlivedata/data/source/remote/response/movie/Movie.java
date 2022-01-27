d8c1e6cd4d6ab30dc61cc8231da4021373185be9
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-01-27 23:58:57 +0700

    Movie.java

---
+++ app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/data/source/remote/response/movie/Movie.java
@@ -0,0 +1,27 @@
+package com.ahmadabuhasan.repositorydanlivedata.data.source.remote.response.movie;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Movie {
+
+    @SerializedName("adult")
+    private
+
+
+    @SerializedName("backdrop_path")
+    private
+
+    @SerializedName("genre_ids")
+    @SerializedName("id")
+    @SerializedName("original_language")
+    @SerializedName("original_title")
+    @SerializedName("original_language")
+    @SerializedName("original_title")
+    @SerializedName("overview")
+    @SerializedName("poster_path")
+    @SerializedName("poster_path")
+    @SerializedName("")
+
+
+
+}
