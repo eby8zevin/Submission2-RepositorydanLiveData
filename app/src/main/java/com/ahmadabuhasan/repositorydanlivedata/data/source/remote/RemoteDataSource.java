60905d83284775cc589d5fea7511064f94e7408b
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-01-28 05:55:03 +0700

    Remote Data Source

--- app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/data/source/remote/RemoteDataSource.java
+++ app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/data/source/remote/RemoteDataSource.java
@@ -1,4 +1,13 @@
 package com.ahmadabuhasan.repositorydanlivedata.data.source.remote;
 
 public class RemoteDataSource {
+
+    private static RemoteDataSource INSTANCE;
+
+    public static RemoteDataSource getINSTANCE() {
+        if (INSTANCE == null){
+            INSTANCE = new RemoteDataSource();
+        }
+        return INSTANCE;
+    }
 }
