cde7de51e9cdaf16b001a4138444f7ebaca5a0ae
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-01-28 05:54:46 +0700

    Movie Response

--- app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/data/source/remote/response/movie/MovieResponse.java
+++ app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/data/source/remote/response/movie/MovieResponse.java
@@ -1,33 +1,20 @@
 package com.ahmadabuhasan.repositorydanlivedata.data.source.remote.response.movie;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
-public class MovieResponse implements Parcelable {
+public class MovieResponse {
 
     @SerializedName("page")
     private int page;
 
     @SerializedName("results")
-    private List<>;
+    private List<Movie> results;
 
     @SerializedName("total_pages")
     private int totalPages;
 
     @SerializedName("total_results")
     private int totalResults;
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel parcel, int i) {
-
-    }
 }
