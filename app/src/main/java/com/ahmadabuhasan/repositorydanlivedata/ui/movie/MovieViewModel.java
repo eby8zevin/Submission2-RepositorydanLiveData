a4f85dc854b4a985f85f33bb93cfc53cdb91ab94
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-03 21:25:33 +0700

    Submission 2

--- app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/ui/movie/MovieViewModel.java
+++ app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/ui/movie/MovieViewModel.java
@@ -1,4 +1,22 @@
 package com.ahmadabuhasan.repositorydanlivedata.ui.movie;
 
-public class MovieViewModel {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.ahmadabuhasan.repositorydanlivedata.data.MovieCatalogueRepository;
+import com.ahmadabuhasan.repositorydanlivedata.data.source.local.entity.MovieEntity;
+
+import java.util.List;
+
+public class MovieViewModel extends ViewModel {
+
+    private MovieCatalogueRepository repository;
+
+    public MovieViewModel(MovieCatalogueRepository mRepository) {
+        this.repository = mRepository;
+    }
+
+    public LiveData<List<MovieEntity>> getMovies() {
+        return repository.getAllMovies();
+    }
 }
