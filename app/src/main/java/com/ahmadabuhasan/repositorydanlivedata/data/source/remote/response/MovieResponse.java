8db4e8d842fa386589055993ca0e863c1eb03cf5
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-03 21:25:26 +0700

    Submission 2

---
+++ app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/data/source/remote/response/MovieResponse.java
@@ -0,0 +1,9 @@
+package com.ahmadabuhasan.repositorydanlivedata.data.source.remote.response;
+
+import com.ahmadabuhasan.repositorydanlivedata.data.source.local.entity.MovieEntity;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class MovieResponse  {
+}
