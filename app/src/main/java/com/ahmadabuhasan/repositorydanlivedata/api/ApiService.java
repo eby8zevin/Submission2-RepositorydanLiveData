c7c3ef81f0bdc33460f3befe9c82561bfddfab77
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-03 21:24:08 +0700

    Submission 2

---
+++ app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/api/ApiService.java
@@ -0,0 +1,14 @@
+package com.ahmadabuhasan.repositorydanlivedata.api;
+
+import com.ahmadabuhasan.repositorydanlivedata.data.source.remote.response.MovieResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiService {
+
+    @GET("discover/movie")
+    Call<MovieResponse> getMovies(
+            @Query("api_key") String apiKey);
+}
