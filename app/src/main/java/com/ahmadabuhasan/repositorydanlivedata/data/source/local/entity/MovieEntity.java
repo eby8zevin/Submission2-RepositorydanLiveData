8cc91ae363ff801a769721b6313a5f962d94f0c6
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-07 03:26:05 +0700

    Submission 2

--- app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/data/source/local/entity/MovieEntity.java
+++ app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/data/source/local/entity/MovieEntity.java
@@ -1,14 +1,14 @@
 package com.ahmadabuhasan.repositorydanlivedata.data.source.local.entity;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 
-public class MovieEntity {
+public class MovieEntity implements Parcelable {
 
     @SerializedName("id")
     private int movieId;
-
-    @SerializedName("overview")
-    private String overview;
 
     @SerializedName("poster_path")
     private String posterPath;
@@ -19,4 +19,67 @@ public class MovieEntity {
     @SerializedName("title")
     private String title;
 
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    protected MovieEntity(Parcel in) {
+        movieId = in.readInt();
+        posterPath = in.readString();
+        releaseDate = in.readString();
+        title = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(movieId);
+        dest.writeString(posterPath);
+        dest.writeString(releaseDate);
+        dest.writeString(title);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<MovieEntity> CREATOR = new Creator<MovieEntity>() {
+        @Override
+        public MovieEntity createFromParcel(Parcel in) {
+            return new MovieEntity(in);
+        }
+
+        @Override
+        public MovieEntity[] newArray(int size) {
+            return new MovieEntity[size];
+        }
+    };
 }
