8c36323d4ec95a4e2f5d237ccecd6c567a8bf872
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-07 03:25:46 +0700

    Submission 2

---
+++ app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/data/source/remote/response/movie/Movie.java
@@ -0,0 +1,162 @@
+package com.ahmadabuhasan.repositorydanlivedata.data.source.remote.response.movie;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Movie {
+
+    @SerializedName("adult")
+    private Boolean adult;
+
+    @SerializedName("backdrop_path")
+    private String backdropPath;
+
+    @SerializedName("genre_ids")
+    private List<Genre> genreIds;
+
+    @SerializedName("id")
+    private int movieId;
+
+    @SerializedName("original_language")
+    private String originalLanguage;
+
+    @SerializedName("original_title")
+    private String originalTitle;
+
+    @SerializedName("overview")
+    private String overview;
+
+    @SerializedName("popularity")
+    private Double popularity;
+
+    @SerializedName("poster_path")
+    private String posterPath;
+
+    @SerializedName("release_date")
+    private String releaseDate;
+
+    @SerializedName("title")
+    private String title;
+
+    @SerializedName("video")
+    private Boolean video;
+
+    @SerializedName("vote_average")
+    private Double voteAverage;
+
+    @SerializedName("vote_count")
+    private int voteCount;
+
+    public Boolean getAdult() {
+        return adult;
+    }
+
+    public void setAdult(Boolean adult) {
+        this.adult = adult;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+
+    public List<Genre> getGenreIds() {
+        return genreIds;
+    }
+
+    public void setGenreIds(List<Genre> genreIds) {
+        this.genreIds = genreIds;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getOriginalLanguage() {
+        return originalLanguage;
+    }
+
+    public void setOriginalLanguage(String originalLanguage) {
+        this.originalLanguage = originalLanguage;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public Double getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(Double popularity) {
+        this.popularity = popularity;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getVideo() {
+        return video;
+    }
+
+    public void setVideo(Boolean video) {
+        this.video = video;
+    }
+
+    public Double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(Double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public int getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(int voteCount) {
+        this.voteCount = voteCount;
+    }
+}
