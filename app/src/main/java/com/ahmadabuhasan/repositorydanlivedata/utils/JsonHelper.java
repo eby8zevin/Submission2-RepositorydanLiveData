ef4b660e21efe168a2000eaa1ff1ce1ab2d1544e
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-07 22:54:37 +0700

    JSON Helper

---
+++ app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/utils/JsonHelper.java
@@ -0,0 +1,65 @@
+package com.ahmadabuhasan.repositorydanlivedata.utils;
+
+import android.content.Context;
+
+import com.ahmadabuhasan.repositorydanlivedata.data.source.remote.response.MovieResponse;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JsonHelper {
+
+    private Context context;
+
+    public JsonHelper(Context context) {
+        this.context = context;
+    }
+
+    private String parsingFileToString(String json) {
+        try {
+            InputStream inputStream = context.getAssets().open(json);
+            byte[] bytes = new byte[inputStream.available()];
+            inputStream.read(bytes);
+            inputStream.close();
+            return new String(bytes);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    public List<MovieResponse> loadMovie() {
+        ArrayList<MovieResponse> list = new ArrayList<>();
+        try {
+            String json = parsingFileToString("Movies.json");
+            JSONObject responseObject = null;
+            if (json != null) {
+                responseObject = new JSONObject(json);
+                JSONArray listArray = responseObject.getJSONArray("movies");
+                for (int i = 0; i < listArray.length(); i++) {
+                    JSONObject movie = listArray.getJSONObject(i);
+
+                    int movieId = movie.getInt("id");
+                    String originalLanguage = movie.getString("original_language");
+                    String overview = movie.getString("overview");
+                    String posterPath = movie.getString("poster_path");
+                    String releaseDate = movie.getString("release_date");
+                    String title = movie.getString("title");
+                    Double voteAverage = movie.getDouble("vote_average");
+
+                    MovieResponse movieResponse = new MovieResponse(movieId, originalLanguage, overview, posterPath, releaseDate, title, voteAverage);
+                    list.add(movieResponse);
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+}
