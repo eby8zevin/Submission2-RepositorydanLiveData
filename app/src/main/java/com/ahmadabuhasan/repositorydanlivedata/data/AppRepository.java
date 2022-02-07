d954c615f66360542a3abcd6e44ce631b3016db4
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-08 01:31:22 +0700

    Repository

--- app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/data/AppRepository.java
+++ app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/data/AppRepository.java
@@ -1,4 +1,51 @@
 package com.ahmadabuhasan.repositorydanlivedata.data;
 
-public class AppRepository {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.ahmadabuhasan.repositorydanlivedata.data.source.local.entity.MovieEntity;
+import com.ahmadabuhasan.repositorydanlivedata.data.source.remote.RemoteDataSource;
+import com.ahmadabuhasan.repositorydanlivedata.data.source.remote.response.MovieResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppRepository implements AppDataSource {
+
+    private volatile static AppRepository INSTANCE = null;
+    private final RemoteDataSource remoteDataSource;
+
+    private AppRepository(RemoteDataSource remoteDataSource) {
+        this.remoteDataSource = remoteDataSource;
+    }
+
+    public static AppRepository getInstance(RemoteDataSource remoteData) {
+        if (INSTANCE == null) {
+            synchronized (AppRepository.class) {
+                INSTANCE = new AppRepository(remoteData);
+            }
+        }
+        return INSTANCE;
+    }
+
+    public LiveData<List<MovieEntity>> getAllMovies() {
+        MutableLiveData<List<MovieEntity>> movieResults = new MutableLiveData<>();
+        remoteDataSource.getMovies(movieResponses -> {
+            ArrayList<MovieEntity> movieList = new ArrayList<>();
+            for (MovieResponse response : movieResponses) {
+                MovieEntity movie = new MovieEntity(
+                        response.getMovieId(),
+                        response.getOriginalLanguage(),
+                        response.getOverview(),
+                        response.getPosterPath(),
+                        response.getReleaseDate(),
+                        response.getTitle(),
+                        response.getVoteAverage());
+
+                movieList.add(movie);
+            }
+            movieResults.postValue(movieList);
+        });
+        return movieResults;
+    }
 }
