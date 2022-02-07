c1910366ca94fe2980fc3968eed880c0cd154406
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-08 01:32:45 +0700

    RemoteDataSource

--- app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/data/source/remote/RemoteDataSource.java
+++ app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/data/source/remote/RemoteDataSource.java
@@ -1,4 +1,36 @@
 package com.ahmadabuhasan.repositorydanlivedata.data.source.remote;
 
+import android.os.Handler;
+import android.os.Looper;
+
+import com.ahmadabuhasan.repositorydanlivedata.data.source.remote.response.MovieResponse;
+import com.ahmadabuhasan.repositorydanlivedata.utils.JsonHelper;
+
+import java.util.List;
+
 public class RemoteDataSource {
+
+    private static RemoteDataSource INSTANCE;
+    private JsonHelper jsonHelper;
+    private Handler handler = new Handler(Looper.getMainLooper());
+    private final long LOADING = 3000;
+
+    private RemoteDataSource(JsonHelper jsonHelper) {
+        this.jsonHelper = jsonHelper;
+    }
+
+    public static RemoteDataSource getInstance(JsonHelper helper) {
+        if (INSTANCE == null) {
+            INSTANCE = new RemoteDataSource(helper);
+        }
+        return INSTANCE;
+    }
+
+    public void getMovies(LoadMoviesCallback callback) {
+        handler.postDelayed(() -> callback.onMoviesReceived(jsonHelper.loadMovie()), LOADING);
+    }
+
+    public interface LoadMoviesCallback {
+        void onMoviesReceived(List<MovieResponse> movieResponses);
+    }
 }
