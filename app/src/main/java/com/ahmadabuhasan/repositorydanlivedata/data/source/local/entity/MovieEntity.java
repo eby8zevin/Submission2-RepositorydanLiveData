2abe02da108dbdde02f572e9c8b8d75fd79920a8
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-07 22:54:59 +0700

    Movie Entity

---
+++ app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/data/source/local/entity/MovieEntity.java
@@ -0,0 +1,78 @@
+package com.ahmadabuhasan.repositorydanlivedata.data.source.local.entity;
+
+public class MovieEntity {
+
+    private int movieId;
+    private String originalLanguage;
+    private String overview;
+    private String posterPath;
+    private String releaseDate;
+    private String title;
+    private Double voteAverage;
+
+    public MovieEntity(int movieId, String originalLanguage, String overview, String posterPath, String releaseDate, String title, Double voteAverage) {
+        this.movieId = movieId;
+        this.originalLanguage = originalLanguage;
+        this.overview = overview;
+        this.posterPath = posterPath;
+        this.releaseDate = releaseDate;
+        this.title = title;
+        this.voteAverage = voteAverage;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getOriginalLanguage() {
+        return originalLanguage;
+    }
+
+    public void setOriginalLanguage(String originalLanguage) {
+        this.originalLanguage = originalLanguage;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(Double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+}
