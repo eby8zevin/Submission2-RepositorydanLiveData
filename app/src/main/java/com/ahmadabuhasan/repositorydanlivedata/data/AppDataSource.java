476fa1aebd503b4bbe37b23d46c1a6585545840d
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-09 06:05:39 +0700

    Data Source

--- app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/data/AppDataSource.java
+++ app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/data/AppDataSource.java
@@ -3,10 +3,13 @@ package com.ahmadabuhasan.repositorydanlivedata.data;
 import androidx.lifecycle.LiveData;
 
 import com.ahmadabuhasan.repositorydanlivedata.data.source.local.entity.MovieEntity;
+import com.ahmadabuhasan.repositorydanlivedata.data.source.local.entity.TVShowEntity;
 
 import java.util.List;
 
 public interface AppDataSource {
 
     LiveData<List<MovieEntity>> getAllMovies();
+
+    LiveData<List<TVShowEntity>> getAllTVShows();
 }
