779f03a9ca994e14b3062f2228421329823d0dde
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-09 21:21:35 +0700

    Detail Movie Activity

---
+++ app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/ui/detail/DetailMovieActivity.java
@@ -0,0 +1,16 @@
+package com.ahmadabuhasan.repositorydanlivedata.ui.detail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ahmadabuhasan.repositorydanlivedata.R;
+
+public class DetailMovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_movie);
+    }
+}
