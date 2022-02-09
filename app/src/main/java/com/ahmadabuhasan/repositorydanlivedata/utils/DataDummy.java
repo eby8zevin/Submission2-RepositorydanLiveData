677a55fdaa0d0130d416f901adc34bad51fc8ab9
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-10 00:36:48 +0700

    Data Dummy

--- app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/utils/DataDummy.java
+++ app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/utils/DataDummy.java
@@ -10,12 +10,12 @@ public class DataDummy {
         ArrayList<MovieEntity> movies = new ArrayList<>();
 
         movies.add(new MovieEntity(
-                634649,
+                "634649",
                 "Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.",
                 "https://image.tmdb.org/t/p/w500/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg",
                 "2021-12-15",
                 "Spider-Man: No Way Home",
-                8.4
+                "8.4"
         ));
         return movies;
     }
