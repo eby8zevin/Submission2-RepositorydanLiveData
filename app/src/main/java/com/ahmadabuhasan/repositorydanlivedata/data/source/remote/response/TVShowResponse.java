adf84f26804a22929a3b10c421d673f3521d917a
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-10 00:38:24 +0700

    TVShow Response

--- app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/data/source/remote/response/TVShowResponse.java
+++ app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/data/source/remote/response/TVShowResponse.java
@@ -5,14 +5,14 @@ import android.os.Parcelable;
 
 public class TVShowResponse implements Parcelable {
 
-    private int tvShowId;
+    private String tvShowId;
     private String overview;
     private String posterPath;
     private String firstAirDate;
     private String title;
-    private Double voteAverage;
+    private String voteAverage;
 
-    public TVShowResponse(int tvShowId, String overview, String posterPath, String firstAirDate, String title, Double voteAverage) {
+    public TVShowResponse(String tvShowId, String overview, String posterPath, String firstAirDate, String title, String voteAverage) {
         this.tvShowId = tvShowId;
         this.overview = overview;
         this.posterPath = posterPath;
@@ -21,11 +21,11 @@ public class TVShowResponse implements Parcelable {
         this.voteAverage = voteAverage;
     }
 
-    public int getTvShowId() {
+    public String getTvShowId() {
         return tvShowId;
     }
 
-    public void setTvShowId(int tvShowId) {
+    public void setTvShowId(String tvShowId) {
         this.tvShowId = tvShowId;
     }
 
@@ -61,40 +61,31 @@ public class TVShowResponse implements Parcelable {
         this.title = title;
     }
 
-    public Double getVoteAverage() {
+    public String getVoteAverage() {
         return voteAverage;
     }
 
-    public void setVoteAverage(Double voteAverage) {
+    public void setVoteAverage(String voteAverage) {
         this.voteAverage = voteAverage;
     }
 
     protected TVShowResponse(Parcel in) {
-        tvShowId = in.readInt();
+        tvShowId = in.readString();
         overview = in.readString();
         posterPath = in.readString();
         firstAirDate = in.readString();
         title = in.readString();
-        if (in.readByte() == 0) {
-            voteAverage = null;
-        } else {
-            voteAverage = in.readDouble();
-        }
+        voteAverage = in.readString();
     }
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
-        dest.writeInt(tvShowId);
+        dest.writeString(tvShowId);
         dest.writeString(overview);
         dest.writeString(posterPath);
         dest.writeString(firstAirDate);
         dest.writeString(title);
-        if (voteAverage == null) {
-            dest.writeByte((byte) 0);
-        } else {
-            dest.writeByte((byte) 1);
-            dest.writeDouble(voteAverage);
-        }
+        dest.writeString(voteAverage);
     }
 
     @Override
