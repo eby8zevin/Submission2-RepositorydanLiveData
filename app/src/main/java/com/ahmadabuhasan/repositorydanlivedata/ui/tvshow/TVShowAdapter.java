4cc8d1ee74136e513f0cfd98cd172506bf7f11ee
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-10 01:42:56 +0700

    TVShow Adapter

--- app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/ui/tvshow/TVShowAdapter.java
+++ app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/ui/tvshow/TVShowAdapter.java
@@ -1,5 +1,6 @@
 package com.ahmadabuhasan.repositorydanlivedata.ui.tvshow;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
@@ -9,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.ahmadabuhasan.repositorydanlivedata.R;
 import com.ahmadabuhasan.repositorydanlivedata.data.source.local.entity.TVShowEntity;
 import com.ahmadabuhasan.repositorydanlivedata.databinding.ItemListBinding;
+import com.ahmadabuhasan.repositorydanlivedata.ui.detail.DetailTVShowActivity;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 
@@ -41,6 +43,8 @@ public class TVShowAdapter extends RecyclerView.Adapter<TVShowAdapter.TVShowView
                 .into(holder.binding.ivPoster);
         holder.binding.tvTitle.setText(tvShowEntity.getTitle());
         holder.binding.tvRelease.setText(tvShowEntity.getFirstAirDate());
+
+        holder.bind(tvShowEntity);
     }
 
     @Override
@@ -54,6 +58,14 @@ public class TVShowAdapter extends RecyclerView.Adapter<TVShowAdapter.TVShowView
         public TVShowViewHolder(@NonNull ItemListBinding binding) {
             super(binding.getRoot());
             this.binding = binding;
+        }
+
+        void bind(TVShowEntity tvShowEntity) {
+            itemView.setOnClickListener(view -> {
+                Intent i = new Intent(itemView.getContext(), DetailTVShowActivity.class);
+                i.putExtra(DetailTVShowActivity.EXTRA_TVSHOW, tvShowEntity.getTvShowId());
+                itemView.getContext().startActivity(i);
+            });
         }
     }
 }
