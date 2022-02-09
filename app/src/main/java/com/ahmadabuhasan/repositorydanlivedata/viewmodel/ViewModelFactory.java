20767cbac198972dee9d5039bfe1b29e14f82d31
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-10 00:38:36 +0700

    View Model Factory

--- app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/viewmodel/ViewModelFactory.java
+++ app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/viewmodel/ViewModelFactory.java
@@ -8,6 +8,7 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.ahmadabuhasan.repositorydanlivedata.data.AppRepository;
 import com.ahmadabuhasan.repositorydanlivedata.di.Injection;
+import com.ahmadabuhasan.repositorydanlivedata.ui.detail.DetailMovieViewModel;
 import com.ahmadabuhasan.repositorydanlivedata.ui.movie.MovieViewModel;
 import com.ahmadabuhasan.repositorydanlivedata.ui.tvshow.TVShowViewModel;
 
@@ -35,6 +36,8 @@ public class ViewModelFactory extends ViewModelProvider.NewInstanceFactory {
             return (T) new MovieViewModel(appRepository);
         } else if (modelClass.isAssignableFrom(TVShowViewModel.class)) {
             return (T) new TVShowViewModel(appRepository);
+        } else if (modelClass.isAssignableFrom(DetailMovieViewModel.class)) {
+            return (T) new DetailMovieViewModel(appRepository);
         }
         throw new IllegalArgumentException("Unknown ViewModel class: " + modelClass.getName());
     }
