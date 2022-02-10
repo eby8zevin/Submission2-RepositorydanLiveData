638dea5ae587911bb2a08e2eddc32550d55c2e02
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-02-10 21:52:38 +0700

    Main Activity

--- app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/ui/MainActivity.java
+++ app/src/main/java/com/ahmadabuhasan/repositorydanlivedata/ui/MainActivity.java
@@ -24,9 +24,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        com.ahmadabuhasan.repositorydanlivedata.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         MainPagerAdapter mainPagerAdapter = new MainPagerAdapter(this);
